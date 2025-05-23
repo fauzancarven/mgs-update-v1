3aef2bb03799aa2264bf3f5844a7b215a7395cf7
fauzancaren <fauzancaren@gmail.com>
2025-05-23 17:36:19 +0700

    update test

--- app/Views/admin/accounting/index.php
+++ app/Views/admin/accounting/index.php
@@ -19,7 +19,7 @@
     <div class="card-header border-bottom-0 px-4 pt-4 bg-white mb-lg-0 mb-2">
         <div class="row">
             <div class="mb-0 col-6 pb-4 order-lg-1">
-                <h4 class="mb-0">Accounting</h4> 
+                <h4 class="mb-0">Accounting Test</h4> 
             </div> 
             <div class="justify-content-end col-6 pb-4 order-lg-3">
                 <button class="btn btn-sm btn-primary px-3 rounded" onclick="add_click()"><i class="ti-plus pe-2"></i>Tambah <span class="d-none d-md-inline-block">Store<span></button>
